d97e9d68c439f9a322a35bf96df77fc13eade3dc
Amogh Wagh <amoghwagh@gmail.com>
2020-03-23 16:47:30 +0530

    Removed expo client

--- client/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++
@@ -1,8 +0,0 @@
-package host.exp.exponent.generated;
-
-// This file is auto-generated. Please don't rename!
-public class DetachBuildConstants {
-
-  public static final String DEVELOPMENT_URL = "expf06a20d3a9d34d27a094d552b520adb6://192.168.1.219:19000";
-
-}
