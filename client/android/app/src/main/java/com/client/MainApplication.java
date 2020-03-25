333474ea6b2c7f60ab175101d031a3513f5caac1
Amogh Wagh <amoghwagh@gmail.com>
2020-03-26 00:37:06 +0530

    Added azure notification hub modules to android

--- client/android/app/src/main/java/com/client/MainApplication.java
+++ client/android/app/src/main/java/com/client/MainApplication.java
@@ -1,9 +1,10 @@
-package com.client;
+package com.testapp;
 
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.azure.reactnative.notificationhub.ReactNativeNotificationHubPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
