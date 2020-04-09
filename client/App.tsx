6a7c71d87150b3773d105e288664fbadbf7d5d78
Amogh Wagh <amoghwagh@gmail.com>
2020-04-09 14:17:15 +0530

    Created Basic Structure

--- client/App.tsx
+++ client/App.tsx
@@ -1,204 +1,118 @@
-import React, {useEffect} from 'react';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
 import {
   SafeAreaView,
+  StyleSheet,
+  ScrollView,
   View,
   Text,
   StatusBar,
-  NativeEventEmitter,
-  TouchableOpacity,
-  StyleSheet,
-  Alert,
 } from 'react-native';
 
-const NotificationHub = require('react-native-azurenotificationhub');
-const PushNotificationEmitter = new NativeEventEmitter(NotificationHub);
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-const EVENT_AZURE_NOTIFICATION_HUB_REGISTERED =
-  'azureNotificationHubRegistered';
-const EVENT_AZURE_NOTIFICATION_HUB_REGISTERED_ERROR =
-  'azureNotificationHubRegisteredError';
-const EVENT_REMOTE_NOTIFICATION_RECEIVED = 'remoteNotificationReceived';
-
-const connectionString =
-  'Endpoint=sb://pushprototype.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=2OwufsMW9TtXr2sbeg1u9VxDeTz+N5s8JZO34tJxzLI='; // The Notification Hub connection string
-const hubName = 'lets-talk-hub'; // The Notification Hub name
-const senderID = '465434081085'; // The Sender ID from the Cloud Messaging tab of the Firebase console
-const tags = []; // The set of tags to subscribe to
-const channelName = ''; // The channel's name
-const channelImportance = 3; // The channel's importance (NotificationManager.IMPORTANCE_DEFAULT = 3)
-// Notes:
-//   1. Setting this value to 4 enables heads-up notification on Android 8
-//   2. On some devices such as Samsung Galaxy, changing this value requires
-//      uninstalling/re-installing the app to take effect.
-const channelShowBadge = true;
-const channelEnableLights = true;
-const channelEnableVibration = true;
-const template = '{"data":{"message":"$(message)"}}'; // Notification hub templates:
-// https://docs.microsoft.com/en-us/azure/notification-hubs/notification-hubs-templates-cross-platform-push-messages
-const templateName = 'Sample Template'; // The template's name
+declare var global: {HermesInternal: null | {}};
 
 const App = () => {
-  const _onAzureNotificationHubRegistered = registrationID => {
-    console.warn('RegistrationID: ' + registrationID);
-  };
-
-  const _onAzureNotificationHubRegisteredError = error => {
-    console.warn('Error: ' + error);
-  };
-
-  const _onRemoteNotification = notification => {
-    console.log(notification);
-  };
-
-  function register() {
-    PushNotificationEmitter.addListener(
-      EVENT_AZURE_NOTIFICATION_HUB_REGISTERED,
-      _onAzureNotificationHubRegistered,
-    );
-    PushNotificationEmitter.addListener(
-      EVENT_AZURE_NOTIFICATION_HUB_REGISTERED_ERROR,
-      _onAzureNotificationHubRegisteredError,
-    );
-
-    NotificationHub.register({
-      connectionString,
-      hubName,
-      senderID,
-      tags,
-      channelName,
-      channelImportance,
-      channelShowBadge,
-      channelEnableLights,
-      channelEnableVibration,
-    })
-      .then(res => console.warn(res))
-      .catch(reason => console.warn(reason));
-  }
-
-  function registerTemplate() {
-    PushNotificationEmitter.addListener(
-      EVENT_AZURE_NOTIFICATION_HUB_REGISTERED,
-      _onAzureNotificationHubRegistered,
-    );
-    PushNotificationEmitter.addListener(
-      EVENT_AZURE_NOTIFICATION_HUB_REGISTERED_ERROR,
-      _onAzureNotificationHubRegisteredError,
-    );
-
-    NotificationHub.registerTemplate({
-      connectionString,
-      hubName,
-      senderID,
-      template,
-      templateName,
-      tags,
-      channelName,
-      channelImportance,
-      channelShowBadge,
-      channelEnableLights,
-      channelEnableVibration,
-    })
-      .then(res => console.warn(res))
-      .catch(reason => console.warn(reason));
-  }
-
-  function getInitialNotification() {
-    NotificationHub.getInitialNotification()
-      .then(res => console.warn(res))
-      .catch(reason => console.warn(reason));
-  }
-
-  function getUUID() {
-    NotificationHub.getUUID(false)
-      .then(res => console.warn(res))
-      .catch(reason => console.warn(reason));
-  }
-
-  function isNotificationEnabledOnOSLevel() {
-    NotificationHub.isNotificationEnabledOnOSLevel()
-      .then(res => console.warn(res))
-      .catch(reason => console.warn(reason));
-  }
-
-  function unregister() {
-    NotificationHub.unregister()
-      .then(res => console.warn(res))
-      .catch(reason => console.warn(reason));
-  }
-
-  function unregisterTemplate() {
-    NotificationHub.unregisterTemplate(templateName)
-      .then(res => console.warn(res))
-      .catch(reason => console.warn(reason));
-  }
-
-  useEffect(() => {
-    PushNotificationEmitter.addListener(
-      EVENT_REMOTE_NOTIFICATION_RECEIVED,
-      _onRemoteNotification,
-    );
-  }, []);
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-      fontSize: 20,
-      textAlign: 'center',
-      margin: 10,
-    },
-    instructions: {
-      textAlign: 'center',
-      color: '#333333',
-      marginBottom: 5,
-    },
-  });
-
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={() => register()}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Register</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => registerTemplate()}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Register Template</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => getInitialNotification()}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Get initial notification</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => getUUID()}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Get UUID</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => isNotificationEnabledOnOSLevel()}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>
-            Check if notification is enabled
-          </Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => unregister()}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Unregister</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => unregisterTemplate()}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Unregister Template</Text>
-        </View>
-      </TouchableOpacity>
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.tsx</Text> to change
+                this screen and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
 
 export default App;
--- client/android/app/src/main/java/com/client/MainActivity.java
+++ client/android/app/src/main/java/com/client/MainActivity.java
@@ -1,4 +1,4 @@
-package com.testapp;
+package com.client;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "test-notif";
+    return "client";
   }
 }
