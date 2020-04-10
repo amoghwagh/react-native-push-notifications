ee283118bd44f52a54a4bc923e7cab0c9abd0617
Amogh Wagh <amoghwagh@gmail.com>
2020-04-11 00:27:48 +0530

    Added functionality to send local notification in the app

--- client/App.tsx
+++ client/App.tsx
@@ -9,59 +9,43 @@
  */
 
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, StatusBar} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  Text,
+  StatusBar,
+  Button,
+} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {LocalNotification} from './src/LocalPushController';
 
 declare var global: {HermesInternal: null | {}};
 
 const App = () => {
+  const handleButtonPress = () => {
+    LocalNotification();
+  };
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <Text>Hello World!</Text>
+        <Text>Press a button to trigger the notification</Text>
+        <View style={styles.defaultView}>
+          <Button
+            title={'Local Push Notification'}
+            onPress={handleButtonPress}
+          />
+        </View>
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  defaultView: {
+    marginTop: 20,
   },
 });
 
